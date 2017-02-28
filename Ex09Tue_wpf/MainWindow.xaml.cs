4c91e3d4d30ab873f27e2ae41bc9856ee5f90c8a
Hedviga <hedv0147@edu.eal.dk>
2017-02-28 09:48:44 +0100

    Add project files.

---
+++ Ex09Tue_wpf/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex09Tue_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPage1_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void buttonPage2_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page2();
+        }
+    }
+}
